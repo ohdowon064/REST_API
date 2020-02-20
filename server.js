68834f5abb3569dfb89112bb2d2431194973c2de
ohdowon <dhehdnjs03@gmail.com>
2020-02-20 19:10:02 +0900

    POST method

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
+// 현재 있는 json 포맷의 데이터를 읽을 수 있도록 한다.
 
-// endpoint 생성
+server.use(bodyParser.json());
+// server가 req.body로 들어오는 모든 json 포맷의 데이터를 읽을 수 있도록 한다.
+
 const users = [
     {
         id : 'dhehdnjs',
@@ -17,10 +21,18 @@ const users = [
 
 // 클라이언트에서 /api/user 주소로 get방식의 요청이 왔을 때
 // user json 데이터를 전송한다.
+// get method를 통해서 get으로 /api/user endpoint에 요청이 왔을 경우 res응답으로 users 데이터를 불러왔다.
 server.get('/api/user', (req, res) => {
     res.json(users);
-    console.log(users, 'data is transfered')
+    console.log(users, 'data is transfered by GET method');
 });
+
+server.post('/api/user', (req, res) => {
+    // user 추가하기
+    users.push(req.body);
+    res.json(users);
+    console.log("user is pushed", users);
+})
 
 server.listen(3000, ()=> {
     console.log('The server is running');
